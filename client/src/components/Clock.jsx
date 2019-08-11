0aee082b3dbbfa6c34971a8e6da2b389a77c0d17
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-11 00:24:16 -0700

    renders clock

--- client/src/components/Clock.jsx
+++ client/src/components/Clock.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>APP RENDER</h1>
-  )
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString()
+    });
+  }
+
+  render() {
+    return (
+      <h1>{this.state.time}</h1>
+    )
+  }
 }
 
-export default App
+export default Clock
