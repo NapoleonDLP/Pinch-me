2350e1523fcca18856519e5116ad35f2486f04e9
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-12 14:54:08 -0700

    adds src tag for digital clock font

--- client/src/components/Clock.jsx
+++ client/src/components/Clock.jsx
@@ -3,30 +3,12 @@ import React from 'react';
 class Clock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      time: new Date().toLocaleString()
-    };
-  }
 
-  componentDidMount() {
-    this.intervalID = setInterval(
-      () => this.tick(), 1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  tick() {
-    this.setState({
-      time: new Date().toLocaleString()
-    });
   }
 
   render() {
     return (
-      <h1>{this.state.time}</h1>
+      <h1>{this.props.time}</h1>
     )
   }
 }
