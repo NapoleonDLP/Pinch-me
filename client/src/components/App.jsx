4a03e23f8f374d906b72d21c55ea41a638d40ca0
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-12 14:52:42 -0700

    renders alarm time selectors

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import Clock from './Clock.jsx';
+import Alarm from './Alarm.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      time: new Date().toLocaleString()
+    };
+  }
+
+  componentDidMount() {
+    this.intervalID = setInterval(
+      () => this.tick(), 1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString()
+    });
   }
 
   render() {
     return (
-      <div id="app">
+      <div id="app" >
         <h1>Pinch Me</h1>
-        <Clock />
+        <Clock time={this.state.time} />
+        <Alarm time={this.state.time} />
       </div>
     )
   }
