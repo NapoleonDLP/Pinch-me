7f9155b734f070be2fddd586f282b49fd4f707f7
NapoleonDLP <napoleon.dlp@gmail.com>
2019-08-12 18:10:07 -0700

    builds state, Posts to DB, renders list based on db result

--- client/src/components/Alarm.jsx
+++ client/src/components/Alarm.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import options from '../helpers.js';
+import Alarms from './AlarmList.jsx';
 
 class Alarm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      hour: null,
-      minute: null,
-      seconds: null,
+      hour: 'hr',
+      minute: 'min',
+      seconds: 'sec',
       ampm: 'am',
+      alarms: [],
     }
     this.selectTime = this.selectTime.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,45 +19,61 @@ class Alarm extends React.Component {
   selectTime(e) {
     console.log(e.target.value);
     console.log(e.target.name);
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   handleSubmit(e) {
+    const form = e.target;
     e.preventDefault();
-    console.log(this.state);
+
+    fetch('http://localhost:3000/setAlarm', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.state),
+    })
+    .then((result) => result.json())
+    .then((result) => this.setState({alarms: this.state.alarms.concat([result])}))
+    .catch((err) => console.log(err));
   }
 
   render() {
     return (
-      <form>
-        <label>
-          <div id="alarmSelector">
-            <select name="hour"onChange={this.selectTime} >
-            <option name="hr" id="hrSelector">hr</option>
-              {
-                options(12).map((option, id) => <option name={option} key={id}>{option}</option>)
-              }
-            </select>
-            <select name="minute" onChange={this.selectTime}>
-              <option name="min" id="minSelector">min</option>
-              {
-                options(59).map((option, id) => <option name={option} key={id}>{option}</option>)
-              }
-            </select>
-            <select name="seconds" onChange={this.selectTime}>
-              <option name="sec" id="secSelector">sec</option>
-              {
-                options(59).map((option, id) => <option name={option} key={id}>{option}</option>)
-              }
-            </select>
-            <select name="ampm" onChange={this.selectTime}>
-              <option name="am">am</option>
-              <option name="pm">pm</option>
-            </select>
-          </div>
-        </label>
-        <input type="submit" name="submit" id="setAlarm" value="Set Alarm" onClick={this.handleSubmit}></input>
-      </form>
+      <div>
+        <form>
+          <label>
+            <div id="alarmSelector">
+              <select name="hour"onChange={this.selectTime} >
+              <option name="hr" id="hrSelector">{this.state.hour}</option>
+                {
+                  options(12).map((option, id) => <option name={option} key={id}>{option}</option>)
+                }
+              </select>
+              <select name="minute" onChange={this.selectTime}>
+                <option name="min" id="minSelector">{this.state.minute}</option>
+                {
+                  options(59).map((option, id) => <option name={option} key={id}>{option}</option>)
+                }
+              </select>
+              <select name="seconds" onChange={this.selectTime}>
+                <option name="sec" id="secSelector">{this.state.seconds}</option>
+                {
+                  options(59).map((option, id) => <option name={option} key={id}>{option}</option>)
+                }
+              </select>
+              <select name="ampm" onChange={this.selectTime}>
+                <option name="am">am</option>
+                <option name="pm">pm</option>
+              </select>
+            </div>
+          </label>
+          <input type="submit" name="submit" id="setAlarm" value="Set Alarm" onClick={this.handleSubmit}></input>
+        </form>
+        <div id="alarms">
+          <Alarms alarms={this.state.alarms} />
+        </div>
+      </div>
     )
   }
 }
